9ec4ea4cedbcc10463aa013c9c966c0e82de07d9
sivan <361125982@qq.com>
2020-04-06 15:38:57 +0800

    init commit

---
+++ TestDemo/WXDemoClass/WXDemoObject.h
@@ -0,0 +1,21 @@
+//
+//  WXDemoObject.h
+//  TestDemo
+//
+//  Created by 吴浠 on 2020/4/6.
+//  Copyright © 2020 吴浠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WXDemoObject : NSObject
+
+@property (nonatomic, copy)NSString *name;
+
+- (void)printName;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestDemo/WXDemoClass/WXDemoObject.m
@@ -0,0 +1,17 @@
+//
+//  WXDemoObject.m
+//  TestDemo
+//
+//  Created by 吴浠 on 2020/4/6.
+//  Copyright © 2020 吴浠. All rights reserved.
+//
+
+#import "WXDemoObject.h"
+
+@implementation WXDemoObject
+
+
+- (void)printName {
+    NSLog(@"my name is Sivanwu");
+}
+@end
